d26ebaadb62de3bc93833aec20e6bf8740bc78c4
Mustafa TAÇYILDIZ <93773148+D1422-Mustafa@users.noreply.github.com>
2022-06-06 23:04:49 +0300

    first commit

---
+++ test_lesson.py
@@ -0,0 +1,7 @@
+# from selenium import webdriver
+# options = webdriver.ChromeOptions()
+# options.add_experimental_option('excludeSwitches', ['enable-logging'])
+# browser = webdriver.Chrome(options=options)
+
+# browser.get('http://localhost:8000/')
+# assert browser.page_source.find("Enter hash here")
